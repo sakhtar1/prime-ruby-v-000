6a2f806124dda56faa6c6ede51aa628607be189e
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 17:05:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(int)
   if int == 1 || int == 0 || int == -1
     false
   else
-    Math.sqrt(int).to_i.downto(2).each {|i| return false if int % i == 0}
+    sqrt(int).to_i.downto(2).each {|i| return false if int % i == 0}
     true
   end
 end
