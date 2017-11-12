8f48a07c11f6c3ecf08e2b54914269970d1441a2
Sharmina Akhtar <sakht318@gmail.com>
2017-11-12 17:07:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 def prime?(int)
-i = 2
-limit = int / i
+  i = 2
+  limit = int / i
 
-if int == 1 || int == 0 || int == -1
-  return false
-end
-while i < limit
-  if int % i == 0
+  if int == 1 || int == 0 || int == -1
     return false
   end
-end
-  return true
+  while i < limit
+    if int % i == 0
+      return false
+    end
+  end
+    return true
 end
